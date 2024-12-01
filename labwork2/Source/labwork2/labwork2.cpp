fba142148699022ea91c26afe5a4a59530d590b7
Melisa <melisaceyhann@gmail.com>
2024-12-01 11:11:16 +0300

    labwork2 folder created again.
    labwork2 folder created again and blueprint to c++ changes made.

---
+++ labwork2/Source/labwork2/labwork2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "labwork2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, labwork2, "labwork2" );
