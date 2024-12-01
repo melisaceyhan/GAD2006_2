fcdbd66ca46eb5ca3ddee8dba87d41541401f464
Melisa <melisaceyhann@gmail.com>
2024-12-01 10:54:08 +0300

    labwork2 deleted.
    Labwork2 deleted because of some errors.

--- labwork2/Intermediate/Build/Win64/x64/labwork2Editor/Development/Engine/SharedPCH.Engine.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.41.34123
-#include "C:/Users/melis/OneDrive/Belgeler/GitHub/GAD2006_2/labwork2/Intermediate/Build/Win64/x64/labwork2Editor/Development/Engine/SharedPCH.Engine.Cpp20.h"
--- labwork2/Intermediate/Build/Win64/x64/labwork2Editor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/melis/OneDrive/Belgeler/GitHub/GAD2006_2/labwork2/Intermediate/Build/Win64/x64/labwork2Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
